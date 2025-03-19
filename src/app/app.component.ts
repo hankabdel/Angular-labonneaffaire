6551e2f7e5b415237247dea526077e8ae45c0480
hankabdel <164871804+hankabdel@users.noreply.github.com>
2025-03-19 07:22:59 +0100

    first-Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ArticleComponent } from "./article/article.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ArticleComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'labonneaffaire';
+  constructor() {}
+  ngOnInit(): void {
+    console.log(" Ici ngOnInit");
+  }
 }
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.css'
+})
+export class ArticleComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+  }
+
+}
