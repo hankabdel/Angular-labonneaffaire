a8bb7c82c7e4b940e9bcde013a18c89889c91cc8
hankabdel <164871804+hankabdel@users.noreply.github.com>
2025-03-19 09:59:50 +0100

    modif

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Ajout de l'import
+
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrl: './article.component.css'
+  styleUrl: './article.component.css',
+  imports: [FormsModule],
+
 })
 export class ArticleComponent implements OnInit {
+titreArticle: string = "Titre de l'article";
+prixArticle: number = 12;
+textAltImg: string = "Titre alternative de l'image";
+totalNbrLike: number = 0;
+comment: string = "Ceci est un commentaire";
+
   constructor() {}
   ngOnInit(): void {
   }
 
+onLike() {
+this.totalNbrLike++; 
+}
 }
